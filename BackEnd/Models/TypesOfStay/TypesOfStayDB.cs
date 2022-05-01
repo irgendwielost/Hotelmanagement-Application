fdfd53223c63ca2679b67cb5b927e4c3f5e780ea
Fabi <75372147+irgendwielost@users.noreply.github.com>
2022-05-01 17:37:12 +0200

    Added Bookingtab frontend and some of databinding

---
+++ BackEnd/Models/TypesOfStay/TypesOfStayDB.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace Hotelmanagement.BackEnd.ViewModels.TypesOfStay;
+
+public class TypesOfStayDB
+{
+    public static DataSet GetDataSetTypesOfStay()
+    {
+        using var db = new Database.Database();
+            
+        try
+        {
+            db.connection.Open();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"table opening error{e}");
+            throw;
+        }
+            
+        try
+        {
+                
+            var adapter = new MySqlDataAdapter("select * from `aufenthaltsarten`", db.connection);
+            DataSet dataSet = new();
+            adapter.Fill(dataSet, "aufenthaltsarten");
+            return dataSet;
+        }
+        catch (Exception ex)
+        {
+            throw;
+        }
+        return null;
+    }
+}
--- FrontEnd/Viewmodels/BookingTab.xaml.cs
+++ FrontEnd/Viewmodels/BookingTab.xaml.cs
@@ -1,4 +1,11 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using Hotelmanagement.BackEnd.Models.Customer;
+using Hotelmanagement.BackEnd.Models.Rooms;
+using Hotelmanagement.BackEnd.ViewModels.TypesOfStay;
 
 namespace Hotelmanagement.FrontEnd.Viewmodels
 {
@@ -7,6 +14,70 @@ namespace Hotelmanagement.FrontEnd.Viewmodels
         public BookingTab()
         {
             InitializeComponent();
+            UpdateRoomComboBox();
+            UpdateCustomerComboBox();
+            UpdateTypeOfStayComboBox();
+        }
+
+        
+        public void UpdateCustomerComboBox()
+        {
+            try
+            {
+                var dataset = CustomerDB.GetDataSetCustomer();
+                 CustomerComboBox.ItemsSource = dataset.Tables[0].DefaultView;   
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Daten zu den Kunden konnten nicht geladen werden.\n" + "Error:" + e.Message);
+                Console.WriteLine(e);
+            }
+        }
+        public void UpdateRoomComboBox()
+        {
+            try
+            {
+                var dataset = RoomsDB.GetDataSetRooms();
+                RoomComboBox.ItemsSource = dataset.Tables[0].DefaultView;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Daten zu den Zimmern konnten nicht geladen werden.\n" +"Error:"+  e.Message);
+            }
+        }
+
+        public void UpdateTypeOfStayComboBox()
+        {
+            try
+            {
+                var dataset = TypesOfStayDB.GetDataSetTypesOfStay();
+                TypeOfStayComboBox.ItemsSource = dataset.Tables[0].DefaultView;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Daten zu den Aufenthaltsarten konnten nicht geladen werden.\n" +"Error:"+  e.Message);
+            }
+        }
+        
+        private void SwitchToCustomer(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Neuer Kunde wird erstellt...");
+        }
+
+        //Make sure that the input is a number
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void RoomComboBox_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var id = RoomComboBox.SelectedValue;
+            Rooms room = RoomsDB.GetRoomById(Int32.Parse(id.ToString() ?? string.Empty));
+            //Room name
+            RoomName.Text = room.Designation;
+            
         }
     }
 }
